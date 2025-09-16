8dffb847193fe2dce4de02847a4a7219180276e4
Manikandan2025 <mmanikandan6930@gmail.com>
2025-09-16 13:31:21 +0530

    images added

--- src/MyProjects/Projects.jsx
+++ src/MyProjects/Projects.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Projects.css";
+import image from  '../assets/myprojects.png'
+import project1 from '../assets/smart classroom.jpg'
+import project2 from '../assets/studentMangement.png'
+import project3 from '../assets/businessContract.png' 
+
+
 const Projects = () => {
   return (
     <div className="projects">
       <div className="project-title">
-        <img src="src\assets\myprojects.png" alt="" />
+        <img src={image} alt="" />
       </div>
       <div className="project-list">
         <div className="project1 project">
-          <img src="src\assets\smart classroom.jpg" alt="" />
+          <img src={project1} alt="" />
           <h2>Smart Classroom Management</h2>
           <p>
             A web application that allows teachers to manage their classrooms
@@ -17,7 +23,7 @@ const Projects = () => {
           </p>
         </div>
         <div className="project2 project">
-          <img src="src\assets\studentMangement.png" alt="" />
+          <img src={project2} alt="" />
           <h2>Student Management</h2>
           <p>
             {" "}
@@ -27,7 +33,7 @@ const Projects = () => {
           </p>
         </div>
         <div className="project3 project">
-          <img src="src\assets\businessContract.png" alt="" />
+          <img src={project3} alt="" />
           <h2>Business Contract Validation</h2>
           <p>
             A Business Contract Validation System leveraging NLP and transformer
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,20 @@
-import React, {  useState } from 'react'
-import './NavBar.css' 
+import React, { useState } from "react";
+import "./NavBar.css";
+import protfolio from "../assets/pngegg.png";
 const NavBar = () => {
-  const[menu,setMenu]=useState("home")
+  const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
-        <img src="src\assets\pngegg.png" className='logo' alt="" />
-        <ul className="navmenu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Projects</li>
-            <li>Contact</li>
-
-        </ul>
-      <div className='nav-connect'>Connect With Me</div>
+      <img src={protfolio} className="logo" alt="" />
+      <ul className="navmenu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect With Me</div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
